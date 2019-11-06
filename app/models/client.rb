5055623233774c4c2fa679199140fbf7710ec9e8
Rafael Pivato <rpivato@gmail.com>
2019-11-06 02:29:49 -0300

    Minimum client validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,10 @@
 class Client < ApplicationRecord
+  enum gender: {
+    male: 'male',
+    female: 'female',
+    other: 'other'
+  }
+  validates :name, presence: true
+  validates :birthdate, presence: true
+  validates :gender, inclusion: { in: %w[male female other] }
 end
